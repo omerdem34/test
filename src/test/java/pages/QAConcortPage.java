41803908ea20e8c5d2bac7e7eee9bccd282016e0
Arslnmrv <arslanmerve1619@gmail.com>
2021-10-30 15:30:57 +0300

    updated

--- src/test/java/pages/QAConcortPage.java
+++ src/test/java/pages/QAConcortPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -37,5 +38,45 @@ public class QAConcortPage {
         Thread.sleep(1000);
         qaConcortPage.loginButonu.click();
     }
+
+//US0010 test locatorleri
+    @FindBy (xpath = "(//a[@class='nav-link'])[2]")
+    public WebElement AnaSayfaRoomsSecenegi;
+
+    public void ConcortHotelRoomsGiris() throws InterruptedException {
+        Driver.getDriver().get(ConfigReader.getProperty("CHQAUrl"));
+        QAConcortPage qaConcortPage = new QAConcortPage();
+        Thread.sleep(1000);
+        qaConcortPage.AnaSayfaRoomsSecenegi.click();
+      Thread.sleep(7000);
+    }
+    //Rooms sayfasi
+
+    @FindBy (xpath = "//h1[text()='Rooms']")
+    public WebElement roomsSayfasiRoomsBasligi;
+
+    @FindBy (id = "checkin_date")
+    public WebElement advancedSearchBasligiCheckinDateBox;
+
+    @FindBy (id = "checkout_date")
+    public WebElement advancedSearchBasligiCheckoutDateBox;
+
+    @FindBy (id = "IDRoomType")
+    public WebElement selectRoomTypeDropdown;
+
+    @FindBy (id = "AdultCount")
+    public WebElement selectAdultCountDropdown;
+
+    @FindBy (id = "ChildrenCount")
+    public WebElement childrenDropdown;
+
+    @FindBy (id = "location")
+    public WebElement locationTextBox;
+
+    @FindBy (xpath = "(//div[@class='form-group'])[7]")
+    public WebElement searchButton;
+
+    @FindBy (linkText = "Book Now ")
+    public WebElement bookNow;
 
 }
--- src/test/java/tests/us0002/Tc_001.java
+++ src/test/java/tests/us0002/Tc_001.java
@@ -1,17 +1,6 @@
 package tests.us0002;
 
-import org.testng.annotations.Test;
-import pages.QAConcortPage;
-import utilities.ConfigReader;
-import utilities.Driver;
-
 public class Tc_001   {
 
-    @Test
-    public void test () throws InterruptedException {
-        Driver.getDriver().get(ConfigReader.getProperty("CHQAUrl"));
-        QAConcortPage qaConcortPage = new QAConcortPage();
-        qaConcortPage.ConcortHotelLogin();
-        Driver.closeDriver();
-    }
+
 }
